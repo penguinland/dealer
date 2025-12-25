f2c2b69f61506c412f34db1886ea257de96ec58d
Alan Davidson <alan.davidson@gmail.com>
2025-12-24 21:35:46 -0500

    pr feedback: return an unsigned char instead of a full int

--- fast_randint.h
+++ fast_randint.h
@@ -1,7 +1,8 @@
 #ifndef FAST_RANDINT_H
 #define FAST_RANDINT_H
 
-// WARNING: the argument on the next line must be at least 1 and at most 52.
-int fast_randint(int max);  // Returns a random integer 0..(max-1)
+// Returns a random integer 0..(max-1)
+// WARNING: `max` must be at least 1 and at most 52.
+unsigned char fast_randint(unsigned char max);
 
 #endif // FAST_RANDINT_H
