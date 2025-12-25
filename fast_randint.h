79c297ddb502c7db7dd10e323e5d437c37b1ba39
Alan Davidson <alan.davidson@gmail.com>
2025-12-24 21:33:48 -0500

    pr feedback: add comments for fast_randint

--- fast_randint.h
+++ fast_randint.h
@@ -1,6 +1,7 @@
 #ifndef FAST_RANDINT_H
 #define FAST_RANDINT_H
 
+// WARNING: the argument on the next line must be at least 1 and at most 52.
 int fast_randint(int max);  // Returns a random integer 0..(max-1)
 
 #endif // FAST_RANDINT_H
